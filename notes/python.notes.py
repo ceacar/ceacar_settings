cb8a00217c9262fa484095aeaeb99008d28e8c1d
ceacar <alex.yonder@yahoo.com>
2019-03-04 21:43:44 -0500

    updated class method, static method

--- notes/python.notes.py
+++ notes/python.notes.py
@@ -277,3 +277,46 @@ class TestWth(unittest.TestCase):
         except Exception as e:
             assert True
 
+
+
+#abstract decorator, class method decorator, static method decorator
+class A(object):
+    def foo(self, x):
+        print "executing foo(%s, %s)" % (self, x)
+
+    @classmethod
+    def class_foo(cls, x):
+        print "executing class_foo(%s, %s)" % (cls, x)
+
+    @staticmethod
+    def static_foo(x):
+        print "executing static_foo(%s)" % x    
+
+a = A()
+
+#normal class method have self passed in as first argument
+a.foo(1)
+# executing foo(<__main__.A object at 0xb7dbef0c>,1)
+#With classmethods, the class of the object instance is implicitly passed as the first argument instead of self.
+
+#class method has class method passed in as first argument
+a.class_foo(1)
+# executing class_foo(<class '__main__.A'>,1)
+#You can also call class_foo using the class. In fact, if you define something to be a classmethod, it is probably because you intend to call it from the class rather than from a class instance. A.foo(1) would have raised a TypeError, but A.class_foo(1) works just fine:
+
+#now we can call without instantiate a class
+A.class_foo(1)
+# executing class_foo(<class '__main__.A'>,1)
+#One use people have found for class methods is to create inheritable alternative constructors.
+
+#staticmethod doesn't have self or cls passed to it, it is just a normal function
+#With staticmethods, neither self (the object instance) nor  cls (the class) is implicitly passed as the first argument. They behave like plain functions except that you can call them from an instance or the class:
+
+a.static_foo(1)
+# executing static_foo(1)
+
+#also, static method can be called without instantiate a class
+A.static_foo('hi')
+# executing static_foo(hi)
+Staticmethods are used to group functions which have some logical connection with a class to the class.
+
